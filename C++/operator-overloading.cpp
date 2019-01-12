6803bab60727717f0c3d8131f856257d7cadcfb3
Daniel-Florin Dosaru <dosarudaniel@gmail.com>
2019-01-12 14:25:17 +0200

    Added operator overloading Solution
    https://www.hackerrank.com/challenges/operator-overloading/problem

---
+++ C++/operator-overloading.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Matrix{
+public:
+  vector<vector<int>> a;
+
+  Matrix() {
+  }
+
+  Matrix(int n, int m) {
+    for (int i = 0; i < n; i++) {
+      vector <int> b;
+      for (int j = 0; j < m; j++) {
+        b.push_back(0);
+      }
+      a.push_back(b);
+    }
+  }
+  friend Matrix operator+(const Matrix &m1, const Matrix &m2);
+};
+
+
+  Matrix operator+(const Matrix& m1, const Matrix& m2) {
+    Matrix res(m1.a.size(), m1.a[0].size());
+    for (int i = 0; i < m1.a.size(); i++) {
+      for (int j = 0; j < m1.a[0].size(); j++) {
+        res.a[i][j] = m1.a[i][j] + m2.a[i][j];
+      }
+    }
+    return res;
+  }
+
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
